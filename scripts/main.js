a759d309eb0bb3c12135fe246abfe2fb8a9e550e
two-devs <web.programmerssng@gmail.com>
2019-06-26 18:04:46 +0300

    skills

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
-  
+  $('.mob-menu-btn').click(function (e) { 
+    $('.mob-menu').css('transform','translate(0)')
+  });
+});
+$('.btn-close').click(function (e) { 
+  $('.mob-menu').css('transform','translateX(100%)')
 });
 AOS.init();
