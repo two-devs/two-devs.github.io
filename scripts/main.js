35540a1255da445da2efcb20f8d14749bbbd3cfb
two-devs <web.programmerssng@gmail.com>
2019-07-03 12:51:41 +0300

    new menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,83 @@
-
- // OPEN and CLOSE menu
- $(document).ready(function () {
-  $('.mob-menu-btn').click(function (e) { 
-    $('.mob-menu').css('transform','translate(0)')
-  });
-});
-$('.btn-close').click(function (e) { 
-  $('.mob-menu').css('transform','translateX(100%)')
+// OPEN and CLOSE menu
+$(document).ready(function () {
+  var count = 0;
+ $('.btn-menu').click(function (e) {
+   
+   count+=3; 
+   if(count%2 >0){
+     
+ $('.line').addClass('clicked');
+     if($('.line').hasClass('clicked')){
+       $('.btn-menu').css({
+         'transform':'translateX(-400px)',
+         'background': '#FE7860'
+       });
+       $('.menu').css({
+         'transform':'translateX(0)',
+         'background': '#FE7860'
+       })
+     }
+     console.log(count)
+   }
+ else if (count%2  == 0){
+   $('.line').removeClass('clicked');
+   $('.btn-menu').css({
+     'transform':'translateX(0)',
+     'background': '#506cf0'
+   });
+   $('.menu').css({
+     'transform':'translateX(100%)',
+     'background': '#506cf0'
+   })
+ }
+ });
 });
 
 AOS.init();//AOS
 //Print words
 
 var options = {
-  strings: [
-    "<p class = 'orange'>TWO-DEVS</p>",
-    "<p class='blue'>FRONT-END</p>",
-    "<p class = 'orange>TWO-DEVS</p>",    
-    "<p class='blue'>BACK-END</p>",
-  ],
-  typeSpeed: 130,
-  backSpeed: 100,
-  backDelay: 900,
-  showCursor: false,
-  cursorChar: '|',
-  autoInsertCss: true,
-  shuffle: false,
-  loop: true,
+ strings: [
+   "<p class = 'orange'>TWO-DEVS</p>",
+   "<p class='blue'>FRONT-END</p>",
+   "<p class = 'orange>TWO-DEVS</p>",    
+   "<p class='blue'>BACK-END</p>",
+ ],
+ typeSpeed: 130,
+ backSpeed: 100,
+ backDelay: 900,
+ showCursor: false,
+ cursorChar: '|',
+ autoInsertCss: true,
+ shuffle: false,
+ loop: true,
 };
 var FirstOptions,SecondOptions;
 FirstOptions = {
-  strings:["<div>TWO </div> DEVS","FRONT END","TWO DEVS","BACK END"],
-  typeSpeed: 210,
-  backSpeed: 110,
-  showCursor: false,
-  cursorChar: '|',
-  loop: true,
-  backDelay: 90
+ strings:["<div>TWO </div> DEVS","FRONT END","TWO DEVS","BACK END"],
+ typeSpeed: 210,
+ backSpeed: 110,
+ showCursor: false,
+ cursorChar: '|',
+ loop: true,
+ backDelay: 90
 };
 SecondOptions = {
-  strings:['JS','PHP','HTML5','CSS3','PYTHON'],
-  typeSpeed: 300,
-  backSpeed: 120,
-  showCursor: false,
-  cursorChar: '|',
-  loop: true,
-  backDelay: 90
+ strings:['JS','PHP','HTML5','CSS3','PYTHON'],
+ typeSpeed: 300,
+ backSpeed: 120,
+ showCursor: false,
+ cursorChar: '|',
+ loop: true,
+ backDelay: 90
 }
 var helloOpt = {
-  strings:['HELLO, WORLD :)'],
-  typeSpeed: 200,
-  backSpeed: 120,
-  showCursor: true,
-  cursorChar: '|',
-  loop: false,
+ strings:['HELLO, WORLD :)'],
+ typeSpeed: 200,
+ backSpeed: 120,
+ showCursor: true,
+ cursorChar: '|',
+ loop: false,
 }
 var typed = new Typed(".typed",options);
 var First = new Typed(".first_p",FirstOptions);
@@ -61,24 +85,24 @@ var Second = new Typed(".second_p",SecondOptions);
 var Hello = new Typed(".hello",helloOpt)
 //----------------------//
 window.onload = function(){
- window.setInterval(function(){
-   var now = new Date();
-   var clock = document.getElementById("clock");
-   if(now.getMinutes()>9){
-   clock.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
-   }
-   else{
-   clock.innerHTML = `${now.getHours()}:0${now.getMinutes()}`;
-   }
- }, 1000);
+window.setInterval(function(){
+  var now = new Date();
+  var clock = document.getElementById("clock");
+  if(now.getMinutes()>9){
+  clock.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
+  }
+  else{
+  clock.innerHTML = `${now.getHours()}:0${now.getMinutes()}`;
+  }
+}, 1000);
 };
 //Detect OS
 $(document).ready(function () {
- var ua = detect.parse(navigator.userAgent);
-   if (ua.os.family =='iOS'){
-    $('.screen').addClass('iOS')
-   }
-   if (ua.os.family == 'Android'){
-      $('.screen').addClass('Android')
-   }
+var ua = detect.parse(navigator.userAgent);
+  if (ua.os.family =='iOS'){
+   $('.screen').addClass('iOS')
+  }
+  if (ua.os.family == 'Android'){
+     $('.screen').addClass('Android')
+  }
 });
